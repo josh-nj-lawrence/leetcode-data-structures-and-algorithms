d8c2b4eaaf1f229ccbdca5a979d86fab5db4ca50
Josh Lawrence <josh.nj.lawrence@gmail.com>
2023-01-21 17:53:09 -0500

    Typo fix

--- arrays-and-strings/leetcode-1480E.py
+++ arrays-and-strings/leetcode-1480E.py
@@ -5,7 +5,7 @@ class Solution:
             runningSum.append(nums[i]+runningSum[-1])
         return runningSum
 
-solution = Solution2()
+solution = Solution()
 print(solution.runningSum([1,2,3,4]))
 
 # Results: 20.75% speed (84 ms) 23.43% (14.2 MB)
