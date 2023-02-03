a9c80f592f572afa5bfd5b605c3a2728f75fa92f
Josh Lawrence <josh.nj.lawrence@gmail.com>
2023-02-02 20:38:14 -0500

    Hashing Q383E

---
+++ hashing/leetcode-383E.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        letters = defaultdict(int)
+        for c in magazine:
+            letters[c] += 1
+        
+        for c in ransomNote:
+            if letters[c] == 0:
+                return False
+            letters[c] -= 1
+        return True
+
+sol = Solution()
+print(sol.canConstruct("aa","ab"))
+
+# Results: 55.89% time (69ms) 48.44% space (14.2MB)
