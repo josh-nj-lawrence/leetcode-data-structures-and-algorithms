267bb3da2c08e7cac4536ebbd12be0314cd492cd
Josh Lawrence <josh.nj.lawrence@gmail.com>
2023-02-17 13:22:20 -0500

    Finished fast slow pointer section

--- linked-lists/fast_and_slow_pointers.py
+++ linked-lists/fast_and_slow_pointers.py
@@ -52,3 +52,23 @@ def get_middle(head):
     return slow.val
 
 print(f"get middle: {get_middle(lst1)}")
+
+# Example 3: Give the head of a linked list and an integer k, 
+# return the kth node from the end
+# For example given 1 - 2 - 3 - 4 - 5 and k = 2 return 4
+def get_k_from_end(head, k):
+    ans = head
+    curr = head
+    for _ in range(k):
+        curr = curr.next
+    while curr:
+        ans = ans.next
+        curr = curr.next
+    return ans
+
+lst2 = build_dll([1,2,3,4,5])
+print(get_k_from_end(lst2,2))
+
+# Time complexity is O(n) as we only need to itterate over the length of the list
+# Space complexity is O(1) as we just need to define two pointers.
+
