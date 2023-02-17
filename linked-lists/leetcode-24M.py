76263f027c88f6f10b44540e18d98d8d8a0ef498
Josh Lawrence <josh.nj.lawrence@gmail.com>
2023-02-17 13:56:46 -0500

    Linked List Q24M

---
+++ linked-lists/leetcode-24M.py
@@ -0,0 +1,10 @@
+from tools.linkedlists import ListNode, dListNode, print_list, build_ll
+from typing import Optional
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
