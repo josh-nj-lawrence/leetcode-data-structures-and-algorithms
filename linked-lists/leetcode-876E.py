07fc4a90f49c8afe18e57c86c07cbc2c29026c0b
Josh Lawrence <josh.nj.lawrence@gmail.com>
2023-02-17 13:22:34 -0500

    Linked List Q876E

---
+++ linked-lists/leetcode-876E.py
@@ -0,0 +1,16 @@
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = head
+        slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+    
+# Results: 62% time (33ms) 93.85% space (13.8MB)
