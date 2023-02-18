b2994b747ebe69eee895dc6bafda90ddf8c441b6
Josh Lawrence <josh.nj.lawrence@gmail.com>
2023-02-18 12:22:26 -0500

    Linked Lists Q2130M

--- linked-lists/leetcode-2130M.py
+++ linked-lists/leetcode-2130M.py
@@ -8,30 +8,36 @@ class ListNode:
 
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        og_head = head
+        # Find middle of array with fast/slow
         fast = head
         slow = head
         while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
-        # Now slow points to the left-middle node. Reverse second half
+        # Now slow points to the left-middle node. 
+
+        # Reverse second half
         prev = None
         curr = slow
-        nextNode = slow.next
-        while curr.next:
+        nextNode = slow
+        while curr:
+            nextNode = nextNode.next
             curr.next = prev
             prev = curr
             curr = nextNode
-            nextNode = curr.next
-        
-        slow.next = curr
-        return og_head
+        # Prev stores second half head
+
+        # Find max twosum
+        ans = 0
+        n1, n2 = head, prev
+        while n1 and n2:
+            ans = max(ans, (n1.val + n2.val))
+            n1 = n1.next
+            n2 = n2.next
+        return ans
 
 lst1 = build_ll([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16])
 sol = Solution()
-print_list(sol.pairSum(lst1))
+print(sol.pairSum(lst1))
 
-#   p
-#     c
-#       n
-# 0,1,2,3,4
+# Results: 57.42% time (978 ms) 73.73% space (45.1 MB)
