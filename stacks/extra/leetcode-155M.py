637cf3d467df06c5207ff7277eaa666eebb99a25
Josh Lawrence <josh.nj.lawrence@gmail.com>
2023-03-08 22:29:27 -0500

    Stacks Q155M

---
+++ stacks/extra/leetcode-155M.py
@@ -0,0 +1,34 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append((val,val))
+            return
+        minval = self.stack[-1][1]
+        self.stack.append((val,min(minval,val)))
+
+    
+    def pop(self) -> None:
+        self.stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+        
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+# Results: 57.25% time (64ms) 20.33% space (18.4MB)
