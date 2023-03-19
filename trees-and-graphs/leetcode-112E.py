d8f26f61bb1cfe5cfe5053bb58db8cb17ad6396d
Josh Lawrence <josh.nj.lawrence@gmail.com>
2023-03-19 16:05:04 -0400

    Trees Q112E

--- trees-and-graphs/leetcode-112E.py
+++ trees-and-graphs/leetcode-112E.py
@@ -14,4 +14,19 @@ class Solution:
             return targetSum == 0
         return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
     
+    def hasPathSum2(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        def dfs(node, curr):
+            if not node:
+                return False
+            if node.left is None and node.right is None:
+                return (curr + node.val) == targetSum
+
+            curr += node.val
+            left = dfs(node.left, curr)
+            right = dfs(node.right, curr)
+            return left or right
+        
+        return dfs(root, 0)
+    
 # Results: Time 67.30% (44ms) 88.67% space(15MB)
+# Results 2: time 74.4% (43ms) 88.7% space (14.9MB) 
