a70bf20bf79cd28334d463204445520b4302ee7f
Josh Lawrence <josh.nj.lawrence@gmail.com>
2023-03-18 23:43:46 -0400

    Trees and graphs Q112E

---
+++ trees-and-graphs/leetcode-112E.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        targetSum -= root.val
+        if not root.left and not root.right:
+            return targetSum == 0
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
+    
+# Results: Time 67.30% (44ms) 88.67% space(15MB)
