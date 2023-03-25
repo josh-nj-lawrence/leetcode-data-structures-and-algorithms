a5efa39ef14ad2ae6d37e1f7afa2ed1c55ed2ec9
Josh Lawrence <josh.nj.lawrence@gmail.com>
2023-03-25 09:46:17 -0400

    Trees Q543E

--- trees-and-graphs/leetcode-543E.py
+++ trees-and-graphs/leetcode-543E.py
@@ -5,4 +5,20 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
+    def __init__(self):
+        self.ans = 0
+
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def helper(node: TreeNode) -> int:
+            # Diameter at any node is len of longest path on left + right
+            # Return longest path on left or right
+            if not node:
+                return 0
+            left = helper(node.left)
+            right = helper(node.right)
+            self.ans = max(self.ans, left+right)
+            return max(left,right)
+        helper(root)
+        return self.ans
+        
+# Results: 72.79% time (45ms) 97.90% space (16.2 MB)
