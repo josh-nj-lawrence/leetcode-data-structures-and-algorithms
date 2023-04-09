aeca8e8cf876333f851c176474a8058cb71336c2
Josh Lawrence <josh.nj.lawrence@gmail.com>
2023-04-09 15:46:04 +0100

    Trees Q103M

--- trees-and-graphs/leetcode-103M.py
+++ trees-and-graphs/leetcode-103M.py
@@ -1,4 +1,4 @@
-from collections import deque()
+from collections import deque
 from typing import Optional, List
 # Definition for a binary tree node.
 class TreeNode:
@@ -8,6 +8,30 @@ class TreeNode:
         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        queue = deque([root,])
-        pass
-    
+        if not root:
+            return []
+        
+        row = deque([root])
+        ans = []
+        dir_flag = True
+
+        while row:
+            curr = len(row)
+            #Append to answer array
+            if dir_flag:
+                # append left -> right
+                ans.append([node.val for node in row])
+                dir_flag = False
+            else:
+                ans.append([node.val for node in reversed(row)])
+                dir_flag = True
+            # Add next row
+            for _ in range(curr):
+                node = row.popleft()
+                if node.left:
+                    row.append(node.left)
+                if node.right:
+                    row.append(node.right)
+        return ans
+
+# Results: time 36.76% (38ms) space 47.33%
